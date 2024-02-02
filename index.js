be6418ef8c3a699400c032a692c1dbc29408ed56
Witzevds <witzvand@student.arteveldehs.be>
2024-02-02 13:14:25 +0100

    push

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ document.addEventListener("DOMContentLoaded", function () {
     $dropdownMenu.classList.toggle("show-dropdown");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var video = document.getElementById("myVideo");
+
+  video.addEventListener("click", function () {
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) {
+      /* Firefox */
+      video.mozRequestFullScreen();
+    } else if (video.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      video.webkitRequestFullscreen();
+    } else if (video.msRequestFullscreen) {
+      /* IE/Edge */
+      video.msRequestFullscreen();
+    }
+  });
+});
